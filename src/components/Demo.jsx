1ee3ce8d8b53dc515111367747b6fb987e193460
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-22 11:42:26 +0530

    articles local storage function implemented

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,15 +1,41 @@
 import { useState, useEffect } from 'react'
 import { copy, linkIcon, loader, tick } from '../assets';
-
+import { useLazyGetSummaryQuery } from '../services/article';
 const Demo = () => {
 
-  const [artcile, setArticle] = useState({
+  const [article, setArticle] = useState({
     url: '',
     summary: '',
   });
 
+  const [allArticles, setAllArticles] = useState([]);
+
+   const [getSummary, { error, isFetching } ] = useLazyGetSummaryQuery();
+
+   useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+    localStorage.getItem('articles')
+    )
+    if(articlesFromLocalStorage){
+      setAllArticles(articlesFromLocalStorage)
+    }
+
+   }, []);
+
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+    const { data } = await getSummary({articleUrl: article.url});
+
+    if(data?.summary){
+      const newArticle = { ...article, summary:data.summary};
+
+      const updatedAllArticles = [newArticle, ...allArticles]
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+      console.log(newArticle);
+      localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
+    }
   }
 
   return (
@@ -27,9 +53,9 @@ const Demo = () => {
           <input
             type="url"
             placeholder='Enter a URL'
-            value={artcile.url}
+            value={article.url}
             onChange={(e) => setArticle({
-              ...artcile, url: e.target.value
+              ...article, url: e.target.value
             })}
             required
             className='url_input peer'
