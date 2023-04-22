e165a7153fa5f02702e083a6b6131c1fad77c6a0
Pranshu Kumar Agrawal <pranshukumar1999@gmail.com>
2023-04-22 11:26:17 +0530

    endpoint for the api created

--- src/services/article.js
+++ src/services/article.js
@@ -25,7 +25,9 @@ export const articleApi = createApi({
     }),
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => `test`
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
         })
     })
 })
+// We want to load this on demand/after sumbit is presses and not as soon as the URL is entered
+export const { useLazyGetSummaryQuery } = articleApi;
